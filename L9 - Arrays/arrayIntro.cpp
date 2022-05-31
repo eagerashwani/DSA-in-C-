428166c314e46867012cf1808487548f8710b841
eagerashwani <akemma1541990@gmail.com>
2022-05-31 15:43:31 +0530

    added

--- L9 - Arrays/arrayIntro.cpp
+++ L9 - Arrays/arrayIntro.cpp
@@ -2,8 +2,48 @@
 using namespace std;
 
 int main(){
-    int number[14];
-    //Garbage value
-    cout<<"Number at 2 idx "<<number[6]<<endl;
+
+    int number[14];  //declare
+
+    //Garbage value milegi
+    cout<<"Number at 2 idx "<<number[6]<<endl; //access krne ke liye
+
+    int second[4] = {1,2,3,4};  //initialize array
+    cout<<"Value at 3 idx "<<second[3]<<endl;
+
+    int third[10] = {3,5};
+    int n = 10;
+    for(int i=0; i<n; i++){
+        cout<<third[i]<<" ";  //: 3 5 0 0 0 0 0 0 0 0
+    }
+
+    int fourth[15] = {0};
+    n=15;
+    cout<<"Printing the array"<<endl;
+    for(int i=0; i<n; i++){
+        cout<<fourth[i]<<" "; //: 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    }
+
+
+    // Initialize with 1 is not possible
+    int fifth[9] = {1};
+    n = 9;
+    cout<<"Printing the array"<<endl;
+    for(int i=0; i<n; i++){
+        cout<<fifth[i]<<" "; //: 1 0 0 0 0 0 0 0 0
+    }
+
+    //Initalize with any element
+    int sixth[10];
+    n = 10;
+    for(int i=0; i<n; i++){
+        sixth[i] = 5;
+    }
+    cout<<"Printing the array"<<endl;
+    for(int i=0; i<n; i++){
+        cout<<sixth[i]<<" ";  //: 5 5 5 5 5 5 5 5 5 5
+    }
+
+
     return 0;
 }
