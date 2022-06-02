c14dc2c14f737059ae3f1df5557dff6ed411fff5
Ashwani Kumar <34451016+eagerashwani@users.noreply.github.com>
2022-06-02 11:28:17 +0530

    prime

---
+++ L5 - Bitwise Operator/prime.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main() {
+   int n;
+   cout<<"Enter number to check prime or not ";
+   cin>>n;
+
+   bool isPrime = 1;
+
+   for(int i=2; i*i<=n; i++){
+       if(n%i == 0){
+           isPrime = 0;
+           break;
+
+       }
+   }
+   if(isPrime == 0){
+       cout<<"Not a Prime number"<<endl;
+   }
+   else{
+       cout<<"Prime number"<<endl;
+   }
+   return 0;
+}
