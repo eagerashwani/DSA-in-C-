89b89c60767a1b8198fb0de25d8845a3a71ca722
Ashwani Kumar <34451016+eagerashwani@users.noreply.github.com>
2022-06-03 17:02:35 +0530

    selectionSort

---
+++ L16 - Selection Sort/selectionSort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ selectionSort(int arr[], int size)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[minIndex] > arr[j])
+            {
+                minIndex = j;
+            }
+        }
+        swap(arr[minIndex], arr[j]);
+    }
+    return arr;
+}
